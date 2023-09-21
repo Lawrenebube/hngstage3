d27ac335badea08c28df9f62f822ee0275f8d18a
Lawrenebube <lawrenebube@gmail.com>
2023-09-21 23:36:27 +0100

    first commit

--- src/auth.js
+++ src/auth.js
@@ -6,7 +6,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const Auth0ProviderWithHistory = ({ children }) => {
   const domain = 'dev-h2pp4b7abjxlbnb2.us.auth0.com';
   const clientId = 'd93LugcnmmdKVafEs5ezj1bxfTfb7qt3';
-  const redirectUri = 'https://hngstage3-beta.vercel.app';
+  const redirectUri = 'https://hngstage3-beta.vercel.app/callback';
 
   return (
     <Auth0Provider
